8ee2075487990d3edcf77f410a44532367241fbe
Etienne Prud’homme <e.e.f.prudhomme@gmail.com>
2024-03-09 16:59:53 -0500

    Add env variables for vagrant
    * .shell/env.d/vagrant.sh: Add environment variables for Vagrant.

---
+++ .shell/env.d/vagrant.sh
@@ -0,0 +1,7 @@
+# -*- mode: sh; sh-shell: sh -*-
+# vim:ft=sh:
+
+# /bin/echo -e '\033[1;32mIn .shell/env.d/vagrant.sh\033[0m'
+
+export VAGRANT_DEFAULT_PROVIDER=libvirt
+# export VAGRANT_HOME=/media/shared/vagrant
